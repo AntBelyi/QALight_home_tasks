3047f2dcf3bb8ba36f2035855b5ded3a2dae9282
Anton_Bilyi <Anton_Bilyi@epam.com>
2026-04-23 21:15:54 +0300

    Third java version implementation

---
+++ src/main/java/ThirdVersion.java
@@ -0,0 +1,13 @@
+public class ThirdVersion {
+
+    public static void main(String[] args) {
+        int age = 22;
+
+        if (age > 18) {
+            System.out.println("Hello ser!");
+        } else {
+            System.out.println("Good buy!");
+        }
+    }
+
+}
